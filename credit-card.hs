fb8c7ed0ce97b11e87c508cd4f0ee538d05ece34
meejah <meejah@meejah.ca>
2019-06-04 09:36:29 -0600

    final

--- credit-card.hs
+++ credit-card.hs
@@ -15,15 +15,23 @@ doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther [n] = [n]
 doubleEveryOther [a, b] = [a * 2, b]
-doubleEveryOther (a:b:rest) = [a * 2, b] ++ (doubleEveryOther rest)
+doubleEveryOther (a:b:rest) = [a, b * 2] ++ (doubleEveryOther rest)
 
 sumDigits :: [Integer] -> Integer
 sumDigits [] = 0
-sumDigits [a] = a
-sumDigits (a:as) = a + (sumDigits as)
+sumDigits (a:as)
+  | a < 10 = a + (sumDigits as)
+  | otherwise = (sumDigits (toDigits a)) + (sumDigits as)
+
+-- sumDigits appears correct, but .. this doesn't work
+validate :: Integer -> Bool
+validate x = if (mod (sumDigits (doubleEveryOther (toDigitsRev x))) 10) == 0 then True else False
+
 
 main = do
      putStrLn $ "foo: " ++ show (toDigitsRev 1234)
      putStrLn $ "foo: " ++ show (toDigits 1234)
      putStrLn $ "foo: " ++ show (doubleEveryOther (toDigits 1234))
      putStrLn $ "foo: " ++ show (sumDigits (doubleEveryOther (toDigits 1234)))
+     putStrLn $ "foo: " ++ show (validate 4012888888881881)
+     putStrLn $ "foo: " ++ show (validate 4012888888881882)
